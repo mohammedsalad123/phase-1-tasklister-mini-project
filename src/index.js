92c2f1ad0c935bbb5201744547a62ef58ae551fb
mohammed <mohammed.salad@student.moringaschool.com>
2023-10-14 05:36:17 -0400

    done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+function submitListener() {
+  const p = document.getElementById('create-task-form')
+  p.addEventListener('submit',function(e){
+    e.preventDefault()
+
+
+  })
+  
+}
+
+
+function handleTodo(todo){
+
+  let p = document.createElement('p');
+  let btn = document.createElement('button')
+  document.addEventListener('click', deleteTodo)
+
+  btn.textContent = 'Delete'
+
+  p.textContent = `${todo} `;
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p);
+
+}
+
+function deleteTodo(e){
+  e.target.parentNode.remove();
+}
